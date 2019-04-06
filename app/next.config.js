147b8576aed8280bf6108b5fe9e28c29e5dfde12
Miles Spoor <git@likeminded.io>
2019-04-06 18:05:06 +0200

    Added basic calendar

--- app/next.config.js
+++ app/next.config.js
@@ -1,1 +1,9 @@
-module.exports = {}
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  },
+});
