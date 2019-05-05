f9beff5c70f5d9da3ccf2baf6f2c85978d98d1af
Miles Spoor <git@likeminded.io>
2019-05-05 21:02:12 +0200

    Added better formatting to event duration text

--- app/components/Event/index.js
+++ app/components/Event/index.js
@@ -9,8 +9,8 @@ import styles from './styles.css';
 
 const Event = ({ label, className, startTime, duration }) => (
   <div className={classNames(styles.event, className)} style={stringToMaterialColor(label).style}>
-    {label}
-    {startTime || duration ? <div className={styles.time}>{ moment(startTime).format('H:mm A') } ({durationToHumanReadable(duration)})</div> : null}
+    <span className={styles.label}>{`${label} `}</span>
+    {startTime || duration ? <span className={styles.time}>{ moment(startTime).format('H:mm A') } ({durationToHumanReadable(duration)})</span> : null}
   </div>
 )
 
