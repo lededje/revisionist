faea74e188f8fc7f0f05fb02e174db09b24ce93e
Miles Spoor <git@likeminded.io>
2019-05-05 20:50:00 +0200

    Moved the duration to humanreadable to utils

--- app/components/Event/index.js
+++ app/components/Event/index.js
@@ -2,29 +2,10 @@ import React from 'react';
 import classNames from 'classnames';
 import moment from 'moment';
 
+import durationToHumanReadable from '../../utils/durationToHumanReadable';
 import stringToMaterialColor from '../../utils/stringToMaterialColor';
 
 import styles from './styles.css';
-
-const durationToHumanReadable = (duration) => {
-  const momentDuration = moment.duration().add(duration, 'seconds');
-
-  let string = '';
-
-  if(momentDuration.get('hours') >= 1) {
-    string += `${momentDuration.get('hours')} hr `
-  }
-
-  if(momentDuration.get('minutes') > 0) {
-    string += `${momentDuration.get('minutes')} `
-    if(momentDuration.get('hours') === 0) {
-      string += 'm'
-    }
-  }
-
-
-  return string.trim();
-}
 
 const Event = ({ label, className, startTime, duration }) => (
   <div className={classNames(styles.event, className)} style={stringToMaterialColor(label).style}>
