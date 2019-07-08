640854cd7646638d2f09bfdc3206a644b089b57c
Miles Spoor <miles.spoor@babylonhealth.com>
2019-07-08 12:04:16 +0200

    Added successful login reducer action

--- app/reducers/userReducer.js
+++ app/reducers/userReducer.js
@@ -2,6 +2,14 @@ const initialState = {};
 
 const userReducer = (state = initialState, action = {}) => {
   switch (action.type) {
+    case 'UPDATE_USER_SUCCESS': {
+      return {
+        ...state,
+        id: action.payload.id,
+        name: action.payload.name,
+        email: action.payload.email,
+      };
+    }
     default:
       return state;
   }
