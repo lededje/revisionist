d7b5875a57b917486ae1168693419741f59e1e81
Miles Spoor <miles.spoor@babylonhealth.com>
2019-07-08 12:02:22 +0200

    Added a read auth endpoint to check sessions

--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -1,9 +1,11 @@
 import crud from '../../utils/crud';
 
+import read from './read';
 import create from './create';
 import patch from './verify';
 
 export default crud({
   create,
+  read,
   patch,
 });
